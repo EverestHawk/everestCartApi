18383ba7c05b5c5708c637452218319c104de01b
Dibas Baral <dibas@transmissionmedia.ca>
2019-11-17 23:36:07 -0500

    Add identityserver4 for client oidc

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -23,27 +23,30 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Services.Implementations;
 using Services.Interfaces;
+using webapi;
+using webapi.Services;
 using WebApi.Interfaces;
 using WebApi.Services;
+using IdentityServer4.AccessTokenValidation;
+
 using static Infrastructure.Helpers.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace WebApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,IHostingEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
 
-        public void ConfigureDevelopmentServices(IServiceCollection services)
-        {
-            //ConfigureTestingServices(services);
-            //using real database
-            ConfigureProductionServices(services);
-        }
+    
 
         public void ConfigureTestingServices(IServiceCollection services)
         {
@@ -58,22 +61,19 @@ namespace WebApi
             ConfigureServices(services);
         }
 
-        public void ConfigureProductionServices(IServiceCollection services)
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
-            //Requires LocalDB, which can be installed with SQL server Express 2016
+            //Catalog DbContext
             services.AddDbContext<CatalogContext>(c =>
-            c.UseSqlServer(Configuration.GetConnectionString("CatalogConnection"), b => b.MigrationsAssembly("Infrastructure")));
+           c.UseSqlServer(Configuration.GetConnectionString("CatalogConnection"), b => b.MigrationsAssembly("Infrastructure")));
 
             //Identity DbContext
             services.AddDbContext<AppIdentityDbContext>(i =>
             i.UseSqlServer(Configuration.GetConnectionString("IdentityConnection"), a => a.MigrationsAssembly("Infrastructure")));
 
-            ConfigureServices(services);
-        }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<AppIdentityDbContext>()
                 .AddDefaultTokenProviders();            
@@ -101,8 +101,46 @@ namespace WebApi
             var authSettings = Configuration.GetSection(nameof(AuthSettings));
             services.Configure<AuthSettings>(authSettings);
 
+            //add cors policy to access angular client
+            services.AddCors(options =>
+            {
+                options.AddPolicy("ecCorsPolicy", corsBuilder =>
+                {
+                    corsBuilder.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .SetIsOriginAllowed(origin => origin == "http://localhost:4200")
+                    .AllowCredentials();
+                });
+            });
+            // add healthcheck services
+            services.AddHealthChecks();
             var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(authSettings[nameof(AuthSettings.SecretKey)]));
 
+
+            //add identity server
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.Authentication.CookieLifetime = TimeSpan.FromMinutes(15);
+            })
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddAspNetIdentity<ApplicationUser>()
+                .AddProfileService<CustomProfileService>();
+
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
             //jwt wire up
             //Get options from app settings
             var jwtIssuerOptionsSettings = Configuration.GetSection(nameof(JwtIssuerOptions));
@@ -129,70 +167,75 @@ namespace WebApi
                 ClockSkew = TimeSpan.Zero
             };
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(configureOptions =>
-            {
-                configureOptions.ClaimsIssuer = jwtIssuerOptionsSettings[nameof(JwtIssuerOptions.Issuer)];
-                configureOptions.TokenValidationParameters = tokenValidationParameters;
-                configureOptions.SaveToken = true;
+            #region JWT Authentication wrapper
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(configureOptions =>
+            //{
+            //    configureOptions.ClaimsIssuer = jwtIssuerOptionsSettings[nameof(JwtIssuerOptions.Issuer)];
+            //    configureOptions.TokenValidationParameters = tokenValidationParameters;
+            //    configureOptions.SaveToken = true;
 
-                configureOptions.Events = new JwtBearerEvents
-                {
-                    OnAuthenticationFailed = context =>
-                    {
-                        if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
-                        {
-                            context.Response.Headers.Add("Token-Expired", "true");
-                        }
-                        return Task.CompletedTask;
-                    }
-                };
-            });
+            //    configureOptions.Events = new JwtBearerEvents
+            //    {
+            //        OnAuthenticationFailed = context =>
+            //        {
+            //            if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+            //            {
+            //                context.Response.Headers.Add("Token-Expired", "true");
+            //            }
+            //            return Task.CompletedTask;
+            //        }
+            //    };
+            //});
 
-            //Api user claim policy
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("ApiUser", policy => policy.RequireClaim(JwtClaimIdentifiers.Role, JwtClaims.ApiAccess));
-            });
+            ////Api user claim policy
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("ApiUser", policy => policy.RequireClaim(JwtClaimIdentifiers.Role, JwtClaims.ApiAccess));
+            //});
 
-            //add identity password requirement
-            var identityBuilder = services.AddIdentityCore<ApplicationUser>(o =>
-            {
-                // configure identity options
-                o.Password.RequireDigit = false;
-                o.Password.RequireLowercase = false;
-                o.Password.RequireUppercase = false;
-                o.Password.RequireNonAlphanumeric = false;
-                o.Password.RequiredLength = 6;
-            });
+            ////add identity password requirement
+            //var identityBuilder = services.AddIdentityCore<ApplicationUser>(o =>
+            //{
+            //    // configure identity options
+            //    o.Password.RequireDigit = false;
+            //    o.Password.RequireLowercase = false;
+            //    o.Password.RequireUppercase = false;
+            //    o.Password.RequireNonAlphanumeric = false;
+            //    o.Password.RequiredLength = 6;
+            //});
 
-            identityBuilder = new IdentityBuilder(identityBuilder.UserType, typeof(IdentityRole), identityBuilder.Services);
-            identityBuilder.AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+            //identityBuilder = new IdentityBuilder(identityBuilder.UserType, typeof(IdentityRole), identityBuilder.Services);
+            //identityBuilder.AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+            #endregion
 
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                       .AddIdentityServerAuthentication(options =>
+                       {
+                           options.Authority = "http://localhost:5555";
+                           options.ApiName = "projects-api";
+                           options.RequireHttpsMetadata = false;
+                       });
             // Add memory cache services
             services.AddMemoryCache();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options=> {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            services.AddCors(options =>
-            {
-                options.AddPolicy("ecCorsPolicy",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials();
-                    });
-            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseCors("ecCorsPolicy");
+            app.UseHealthChecks("/health");
 
             app.UseAuthentication();
 
